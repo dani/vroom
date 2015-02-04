90267dd2e904e25da93b4a76608e45049d2c794c
Daniel Berteaud <daniel@firewall-services.com>
2015-02-04 23:10:29 +0100

    HTTP code for set_persistent

--- vroom.pl
+++ vroom.pl
@@ -1526,8 +1526,10 @@ any '/api' => sub {
     return $self->render(
       json => {
         msg    => $self->l('ERROR_OCCURRED'),
+        err    => 'ERROR_OCCURRED',
         status => 'error'
-      }
+      },
+      status => 503
     );
   }
   # Set/unset askForName
