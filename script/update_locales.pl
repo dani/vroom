04885aa0d07d6b4252ba0ebc80b63b5fff78c718
Daniel Berteaud <daniel@firewall-services.com>
2015-09-18 14:14:39 +0200

    Update main script name

--- script/update_locales.pl
+++ script/update_locales.pl
@@ -7,7 +7,7 @@ use Locale::Maketext::Extract::Run 'xgettext';
 
 chdir dirname($0) . '/..';
 
-my @files = qw(vroom.pl public/js/vroom.js);
+my @files = qw(vroom public/js/vroom.js);
 push @files, glob('templates/default/*');
 
 foreach my $lang (map { basename(s/\.po$//r) } glob('lib/Vroom/I18N/*.po')){
